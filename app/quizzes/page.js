0c95185388e861ca419547c08ce332b396894958
yafiethaile <120148011+yafiethaile@users.noreply.github.com>
2023-11-27 18:49:31 -0700

    .

---
+++ app/quizzes/page.js
@@ -0,0 +1,11 @@
+import Quiz from './quiz'
+
+export default function easyQuizPage () {
+    return (
+    <div>
+        <h1 className="text-4xl font-bold text-center">Easy Quiz </h1>
+        <Quiz />
+        
+    </div>
+    );
+}
